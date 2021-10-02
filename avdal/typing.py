436e838ff27fb2ae27359bf6202a7cc3bc3f33cb
Hazim Avdal <dev@avd.al>
2021-10-02 00:16:18 -0500

    added typing.py

---
+++ avdal/typing.py
@@ -0,0 +1,35 @@
+import inspect
+import typing as t
+from inspect import Parameter
+
+
+def enforce_type_annotations():
+    def decorator(f: t.Callable) -> t.Callable:
+        def wrapper(*args, **kwargs):
+            checks = {}
+            sig = inspect.signature(f)
+            i = 0
+            for k, v in sig.parameters.items():
+                if k in kwargs:
+                    checks[k] = (v.annotation, kwargs[k])
+                else:
+                    checks[k] = (v.annotation, args[i])
+                    i += 1
+
+            for arg, (annotation, value) in checks.items():
+                if annotation == Parameter.empty:
+                    continue
+
+                if annotation != type(value):
+                    fn = f.__name__
+                    e = annotation.__name__
+                    a = type(value).__name__
+
+                    raise ValueError(f"{fn}: param {arg} expects a value of type {e}, got {a}")
+
+            return f(*args, **kwargs)
+
+        wrapper.__name__ = f.__name__
+        return wrapper
+
+    return decorator
