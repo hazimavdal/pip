fbb277138fede26858a4faa3a11e6419333afd6b
Hazim Avdal <dev@avd.al>
2021-10-04 00:03:05 -0500

    added rbac.py

--- tests/test_avdal.py
+++ tests/test_avdal.py
@@ -1,5 +1,6 @@
 import unittest
 from avdal import annotations
+from avdal import rbac
 
 
 def fails(f, *args, **kwargs):
@@ -52,3 +53,45 @@ class TestTyping(unittest.TestCase):
         self.assertTrue(getattr(a, "d"), 7)
         self.assertTrue(getattr(a, "e"), 5)
         self.assertTrue(getattr(a, "f"), 8)
+
+
+class TestRbac(unittest.TestCase):
+    def test_permset(self):
+        r1 = rbac.PermSet("rn:a:b", "read")
+
+        self.assertTrue(r1.check("rn:a:b", {"read"}))
+        self.assertTrue(r1.check("rn:a:b", {"*"}))
+        self.assertFalse(r1.check("rn:a:b", {"write"}))
+        self.assertFalse(r1.check("rn:a:b:c", {"read"}))
+        self.assertFalse(r1.check("rn:a:b:c", {"write"}))
+        self.assertFalse(r1.check("rn:a", {"write"}))
+        self.assertFalse(r1.check("rn:a:*", {"write"}))
+        self.assertTrue(r1.check("rn:a:*", {"read"}))
+        self.assertTrue(r1.check("rn:*", {"read"}))
+        self.assertTrue(r1.check("*", {"read"}))
+        self.assertFalse(r1.check("*", {"write"}))
+        self.assertTrue(r1.check("*", {"*"}))
+
+
+        r2 = rbac.PermSet("rn:a:b", "write")
+
+        self.assertFalse(r2.check("rn:a:b", {"read"}))
+        self.assertTrue(r2.check("rn:a:b", {"write"}))
+        self.assertTrue(r2.check("rn:a:b", {"write", "read"}))
+        self.assertTrue(r2.check("rn:a:b", {"update", "write", "read"}))
+        self.assertFalse(r2.check("rn:a:b", {"update"}))
+        self.assertFalse(r2.check("rn:a:b", set()))
+        self.assertTrue(r2.check("rn:a:b", {"*"}))
+        self.assertTrue(r2.check("rn:a:b", {"*", "read"}))
+        self.assertTrue(r2.check("rn:a:b", {"*", "write"}))
+        self.assertTrue(r2.check("rn:a:b", {"*", "update", "read"}))
+        self.assertFalse(r2.check("rn:a:b:c", {"*", "update", "read"}))
+        self.assertTrue(r2.check("rn:a:b:*", {"*", "update", "read"}))
+
+        r3 = rbac.PermSet("rn:a:b", "read")
+        self.assertTrue(r3.check("rn:a:*", {"read"}))
+        self.assertTrue(r3.check("rn:a:b:*", {"read"}))
+        self.assertFalse(r3.check("rn:a:b:c", {"read"}))
+        self.assertFalse(r3.check("rn:a:c", {"read"}))
+        self.assertFalse(r3.check("rn:a", {"read"}))
+
