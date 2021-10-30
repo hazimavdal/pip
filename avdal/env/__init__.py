ddba8059f7c97374f902e8b0cb5c65cd24115340
Hazim Avdal <dev@avd.al>
2021-10-30 17:29:31 -0400

    u

--- avdal/env/__init__.py
+++ avdal/env/__init__.py
@@ -1,7 +1,7 @@
 import os
 import re
 from typing import List
-from collections.abc import Mapping
+from collections.abc import Mapping, MutableMapping
 
 _envre = re.compile(r'''^(?:export\s*)?([_a-zA-Z][\w_]*)\s*=\s*(.*)$''')
 _varexp = re.compile(r'''\{\{(.*?)\}\}''')
@@ -9,15 +9,29 @@ _varname = re.compile(r'''^\s*([\w_]+)\s*$''')
 _include_re = re.compile(r'''^#include\s+(.*)\s*$''')
 
 
-class Environment(Mapping):
-    def __init__(self, data: Mapping):
-        self._data = data
+class Environment(MutableMapping):
+    def __init__(self, *args, **kwargs):
+        self.__dict__.update(*args, **kwargs)
+
+    def _expand(self, value):
+        for var in _varexp.findall(value):
+            match = _varname.match(var)
+            if not match:
+                raise Exception(f"[{var}]: invalid variable name")
+
+            varname = match.group(1)
+            if varname not in self:
+                raise Exception(f"[{varname}]: unbounded variable")
+
+            value = value.replace(f"{{{{{var}}}}}", self[varname])
+
+        return value
 
     def union(self, other: Mapping):
         return Environment({**self, **other})
 
     def get(self, key: str, default=None, nullable=False, mapper=lambda x: x):
-        value = self._data.get(key)
+        value = self.__dict__.get(key)
 
         if value is not None:
             return mapper(value)
@@ -27,36 +41,28 @@ class Environment(Mapping):
 
         return mapper(default)
 
-    def __getitem__(self, k):
-        return self._data[k]
+    def __setitem__(self, key, value):
+        self.__dict__[key] = self._expand(value)
+
+    def __getitem__(self, key):
+        return self.__dict__[key]
+
+    def __delitem__(self, key):
+        del self.__dict__[key]
 
     def __iter__(self):
-        return (k for k in self._data)
+        return iter(self.__dict__)
 
     def __len__(self):
-        return len(self._data)
+        return len(self.__dict__)
 
     def __repr__(self) -> str:
         return "{}({{{}}})".format(type(self).__name__, ', '.join(
             ('{!r}: {!r}'.format(key, value)
-             for key, value in self._data.items())))
+             for key, value in self.__dict__.items())))
 
 
 class DotEnv(Environment):
-    def _expandvars(self, value):
-        for var in _varexp.findall(value):
-            match = _varname.match(var)
-            if not match:
-                raise Exception(f"[{var}]: invalid variable name")
-
-            varname = match.group(1)
-            if varname not in self.vars:
-                raise Exception(f"[{varname}]: unbounded variable")
-
-            value = value.replace(f"{{{{{var}}}}}", self.vars.get(varname))
-
-        return value
-
     def _load_env(self, env_file: str):
         env_file = os.path.abspath(env_file)
 
@@ -80,13 +86,13 @@ class DotEnv(Environment):
                     key = match.group(1)
                     value = match.group(2).strip('"').strip("'")
 
-                    self.vars[key] = self._expandvars(value)
+                    self[key] = value
+
+        return vars
 
     def __init__(self, *env_files):
-        self.vars = {}
+        super(DotEnv, self).__init__()
         self.envs = set()
 
         for file in env_files:
             self._load_env(file)
-
-        super(DotEnv, self).__init__(self.vars)
