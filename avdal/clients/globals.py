7e4042292b1681b17696c5ad9e8daf364e2fe61a
Hazim Avdal <dev@avd.al>
2022-05-25 14:43:18 -0400

    u

--- avdal/clients/globals.py
+++ avdal/clients/globals.py
@@ -6,7 +6,6 @@ PERMA_URL = "https://avd.al/globals.json"
 class Globals:
     def __init__(self):
         self.globals = requests.get(PERMA_URL).json()
-        self.globals = self.globals["globals"]
         self.vars = self.globals["vars"]
 
     def get_var(self, name, default=None):
