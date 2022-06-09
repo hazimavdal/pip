b77a605e32730b98b0f70883ee222d38e092c162
Hazim Avdal <dev@avd.al>
2022-06-09 01:36:20 -0400

    u

--- avdal/aes.py
+++ avdal/aes.py
@@ -1,7 +1,10 @@
 import struct
 import secrets
+import hashlib
 from typing import Tuple
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 
 class AES:
@@ -28,15 +31,27 @@ class AES:
 
         return plaintext
 
-    def encrypt(self, plaintext: bytes) -> Tuple[bytes, bytes]:
+    def key_rand(self):
+        return self.__entropy__(AES.AES_BLOCKSIZE)
+
+    def key_pbkdf2(self, password: bytes, iterations: int = 65536) -> bytes:
+        kdf = PBKDF2HMAC(
+            algorithm=hashes.SHA256(),
+            length=AES.AES_BLOCKSIZE,
+            salt=hashlib.sha256(password).digest(),
+            iterations=iterations,
+        )
+
+        return kdf.derive(password)
+
+    def encrypt(self, key: bytes, plaintext: bytes) -> Tuple[bytes, bytes]:
         iv = self.__entropy__(AES.IV_SIZE)
-        key = self.__entropy__(AES.AES_BLOCKSIZE)
         padded = self.__pad__(plaintext)
 
         encryptor = Cipher(algorithms.AES(key), modes.CBC(iv)).encryptor()
         ciphertext = encryptor.update(padded) + encryptor.finalize()
 
-        return key, self.__pack__(len(plaintext), iv, ciphertext)
+        return self.__pack__(len(plaintext), iv, ciphertext)
 
     def decrypt(self, key: bytes, ciphertext: bytes) -> bytes:
         size, iv, body = self.__unpack__(ciphertext)
